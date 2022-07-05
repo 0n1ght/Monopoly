9a9e304b62bf0819a7d309c81ea591c6627b872b
karol <karol05ks@gmail.com>
2022-07-05 11:48:46 +0200

    player horizontal left line move

--- src/main/java/pl/monopoly/Player.java
+++ src/main/java/pl/monopoly/Player.java
@@ -1,19 +1,24 @@
 package pl.monopoly;
 
 public class Player {
-    private int field = 0;
+    private int field;
     private static int nextId = 0;
     private int id;
 
     // create
     public Player() {
         id = nextId;
+        field = id+9;
         nextId++;
     }
 
     // get/set
     public int getId() {
         return id;
+    }
+
+    public int getField() {
+        return field;
     }
 
 }
--- src/main/java/pl/monopoly/PlayerView.java
+++ src/main/java/pl/monopoly/PlayerView.java
@@ -9,11 +9,23 @@ public class PlayerView {
     public void render(Graphics graphics) {
         graphics.setColor(colors[player.getId()]);
 
-        if (player.getId() == 0) {
-            graphics.fillOval(770, 795, 50, 50);
-        } else if (player.getId() == 1) {
-            graphics.fillOval(825, 795, 50, 50);
+        int distance = 73 * player.getField();
+
+        if (player.getField() > 0) {
+            distance += 15;
         }
+
+        if (player.getField() >= 10) {
+            distance += 15;
+        }
+
+        graphics.fillOval(790+10*player.getId() - distance, 795, 50, 50);
+
+//        if (player.getId() == 0) {
+//            graphics.fillOval(770, 795, 50, 50);
+//        } else if (player.getId() == 1) {
+//            graphics.fillOval(825, 795, 50, 50);
+//        }
 
     }
 
