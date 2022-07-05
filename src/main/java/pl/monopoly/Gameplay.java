f20107f67b5436a80de77ec35c6acb2955fafb53
karol <karol05ks@gmail.com>
2022-07-05 10:59:10 +0200

    players spawn

--- src/main/java/pl/monopoly/Gameplay.java
+++ src/main/java/pl/monopoly/Gameplay.java
@@ -4,11 +4,20 @@ import java.awt.*;
 
 public class Gameplay {
     public void tick() {
+
         System.out.println("start");
+
     }
 
     public void render(Graphics graphics) {
+
         Image image = Toolkit.getDefaultToolkit().getImage("src\\main\\resources\\monopoly-plansza.jpg");
         graphics.drawImage(image, 0, 0, null);
+
+        graphics.setColor(Color.blue);
+        graphics.fillOval(770, 795, 50, 50);
+        graphics.setColor(Color.red);
+        graphics.fillOval(825, 795, 50, 50);
+
     }
 }
