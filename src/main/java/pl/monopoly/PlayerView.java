65f8e53164412708a69ac30f1bb2ced5e8f65c4b
karol <karol05ks@gmail.com>
2022-07-07 10:08:30 +0200

    mini refactoring of player move by field

--- src/main/java/pl/monopoly/PlayerView.java
+++ src/main/java/pl/monopoly/PlayerView.java
@@ -9,7 +9,7 @@ public class PlayerView {
     public void render(Graphics graphics) {
         graphics.setColor(colors[player.getId()]);
 
-        int distance1 = 0, distance2 = 0;
+        int distance1=0, distance2=0;
 
         if (player.getField() > 40) {
             player.setField(player.getField()-40);
@@ -18,7 +18,6 @@ public class PlayerView {
         if (player.getField() <= 10) {
 
             distance1 = - 73 * player.getField();
-            distance2 = 0;
 
         } else if (player.getField() > 10 && player.getField() <= 20) {
 
@@ -32,7 +31,6 @@ public class PlayerView {
 
         } else if (player.getField() > 30) {
 
-            distance1 = 0;
             distance2 = -73 * 10 + 73 * (player.getField() - 30);
 
         }
@@ -50,29 +48,3 @@ public class PlayerView {
     }
 
 }
-
-/*
-
-    int HorizontalField = player.getField() > 10 ? player.getField()-10 : 0;
-    int VerticalField = Math.min(player.getField(), 10);
-    int distance1, distance2;
-
-    if (player.getField() <= 20
-            distance1 = 73 * (player.getField()-HorizontalField);
-            distance2 = 73 * (player.getField()-VerticalFiel
-            } else if (player.getField() > 20 && player.getField() <= 3
-            distance1 = 73 * 10 - player.getField() * 73;
-            distance2 = 73 * 1
-            } els
-            distance1 = 0;
-            distance2 =
-
-            if (player.getField() > 0) {distance1 += 15;}
-            if (player.getField() >= 10) {distance1 += 15;}
-            if (player.getField() > 10) {distance2 += 15;}
-            if (player.getField() >= 20) {distance2 += 15;}
-            if (player.getField() > 20) {distance1 -= 15;}
-            if (player.getField() >= 30) {distance1 -= 15;}
-            if (player.getField() > 30) {distance2 -= 15;}
-            graphics.fillOval(790+10*player.getId() - distance1, 790+10*player.getId() - distance2, 50, 50);
-            */
