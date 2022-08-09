2ab9e1373fe48c8433852c98613dc63f94e3a935
Karol Kasperek <karol05ks@gmail.com>
2022-08-09 11:56:27 +0200

    -global code refactoring

--- src/main/java/pl/monopoly/logic/Game.java
+++ src/main/java/pl/monopoly/logic/Game.java
@@ -3,10 +3,8 @@ package pl.monopoly.logic;
 import pl.monopoly.view.BoardView;
 import pl.monopoly.view.Display;
 
-import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
 public class Game {
    private Player player1, player2, player3, player4;
@@ -14,7 +12,6 @@ public class Game {
     private final BoardView boardView = new BoardView();
     private LinkedList<Player> queue;
     public static int playersNumber = 4;
-    private static boolean checkedPlayersNumber = false;
     private static Game game = null;
 
     public Game() {
@@ -28,24 +25,6 @@ public class Game {
     }
 
     public Player actualPlayer() {
-
-      /*  if (!checkedPlayersNumber) {
-            checkedPlayersNumber = true;
-            switch (playersNumber) {
-                case 2 -> {queue.removeLast(); queue.removeLast();}
-                case 3 -> queue.removeLast();
-            }
-        }*/
-
-      /*  if (queue.isEmpty()) {
-            switch (playersNumber) {
-                case 2 -> queue = new LinkedList<>(List.of(player1, player2));
-                case 3 -> queue = new LinkedList<>(List.of(player1, player2, player3));
-                case 4 -> queue = new LinkedList<>(List.of(player1, player2, player3, player4));
-            }
-
-            return queue.peek();
-        }*/
         return queue.peek();
     }
 
@@ -59,7 +38,6 @@ public class Game {
         Display.refreshScoreBoard();
         Player actual = queue.remove();
         queue.add(actual);
-        System.out.println(queue);
     }
 
     // get/set
