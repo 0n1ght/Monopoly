481aa5139961edd1e50b7441461247c2804f35a9
Karol Kasperek <karol05ks@gmail.com>
2022-10-11 20:58:02 +0200

    -FieldFactory refactoring

--- src/main/java/pl/monopoly/logic/FieldFactory.java
+++ src/main/java/pl/monopoly/logic/FieldFactory.java
@@ -2,11 +2,10 @@ package pl.monopoly.logic;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class FieldFactory {
-    private Game game;
+    private final Game game;
 
     public FieldFactory(Game game) {
         this.game = game;
@@ -14,17 +13,13 @@ public class FieldFactory {
 
     public List<Field> buildFields() throws IOException {
         List<Field> fields = new ArrayList<>();
-        int fieldNumber, price, tax;
-        boolean buyAble;
-        FieldSet fieldSet;
 
         BufferedReader bufferedWriter = new BufferedReader(new FileReader("src\\main\\resources\\fields.csv"));
 
         for (int i = 0; i < 40; i++) {
+
             String csv = bufferedWriter.readLine();
             String[] split = csv.split(",");
-
-            System.out.println(Arrays.toString(split));
 
             if (split[1].equals("BUY")) {
                 fields.add(new BuyAbleField(game, FieldSet.valueOf(split[4]), Integer.parseInt(split[5]), Integer.parseInt(split[2]), Integer.parseInt(split[3])));
