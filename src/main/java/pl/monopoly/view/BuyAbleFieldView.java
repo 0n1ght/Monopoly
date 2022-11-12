9086496531f6396e570083b2d1bb316a01c5a41a
karol <karol05ks@gmail.com>
2022-11-12 19:43:03 +0100

    -creating BuyAbleFieldView

--- src/main/java/pl/monopoly/view/BuyAbleFieldView.java
+++ src/main/java/pl/monopoly/view/BuyAbleFieldView.java
@@ -6,14 +6,10 @@ import pl.monopoly.logic.BuyAbleField;
 import javax.swing.*;
 import java.awt.*;
 
-public class BuyAbleFieldView {
-    private final BuyAbleField field;
-
-    public BuyAbleFieldView(BuyAbleField field) {
-        this.field = field;
-    }
+public record BuyAbleFieldView(BuyAbleField field) {
 
     public void render(Graphics g) {
+//        ImageIcon crossImage = new ImageIcon("src\\main\\java\\pl\\monopoly\\images\\X_marks\\X_red.png");
         ImageIcon crossImage = switch (field.getOwner().getId()) {
 
             case 0 -> new ImageIcon("src\\main\\java\\pl\\monopoly\\images\\X_marks\\X_red.png");
@@ -28,12 +24,12 @@ public class BuyAbleFieldView {
 
         if (field.getNumber() <= 10) {
 
-            distance1 = - normalField * field.getNumber();
+            distance1 = -normalField * field.getNumber();
 
         } else if (field.getNumber() > 10 && field.getNumber() <= 20) {
 
-            distance1 = - normalField * 10;
-            distance2 = - normalField * (field.getNumber() - 10);
+            distance1 = -normalField * 10;
+            distance2 = -normalField * (field.getNumber() - 10);
 
         } else if (field.getNumber() > 20 && field.getNumber() <= 30) {
 
@@ -46,14 +42,28 @@ public class BuyAbleFieldView {
 
         }
 
-        if (field.getNumber() > 0) {distance1 -= bigFieldAddition;}
-        if (field.getNumber() >= 10) {distance1 -= bigFieldAddition;}
-        if (field.getNumber() > 10) {distance2 -= bigFieldAddition;}
-        if (field.getNumber() >= 20) {distance2 -= bigFieldAddition;}
-        if (field.getNumber() > 20) {distance1 += bigFieldAddition;}
-        if (field.getNumber() >= 30) {distance1 += bigFieldAddition;}
-        if (field.getNumber() > 30) {distance2 += bigFieldAddition;}
+        if (field.getNumber() > 0) {
+            distance1 -= bigFieldAddition;
+        }
+        if (field.getNumber() >= 10) {
+            distance1 -= bigFieldAddition;
+        }
+        if (field.getNumber() > 10) {
+            distance2 -= bigFieldAddition;
+        }
+        if (field.getNumber() >= 20) {
+            distance2 -= bigFieldAddition;
+        }
+        if (field.getNumber() > 20) {
+            distance1 += bigFieldAddition;
+        }
+        if (field.getNumber() >= 30) {
+            distance1 += bigFieldAddition;
+        }
+        if (field.getNumber() > 30) {
+            distance2 += bigFieldAddition;
+        }
 
-        g.drawImage(crossImage.getImage(), 790 + distance1, 790 + distance2, 100, 100, null);
+        g.drawImage(crossImage.getImage(), 760 + distance1, 775 + distance2, 100, 100, null);
     }
 }
